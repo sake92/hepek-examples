d366f3d5bd9b4f9f697d2034d5d9d97707326938
Sakib Hadziavdic <sakib@sake.ba>
2023-10-26 18:02:32 +0200

    Use sdkman for netlify

--- netlify-build.sh
+++ netlify-build.sh
@@ -3,8 +3,8 @@ curl -s "https://get.sdkman.io" | bash
 
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-sdk install java 11
-sdk use java 11
+sdk install java 11.0.20-tem
+sdk use java 11.0.20-tem
 
 sdk install sbt 1.9.7
 sdk use sbt 1.9.7
